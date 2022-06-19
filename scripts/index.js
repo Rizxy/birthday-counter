5b02c42c9be7875cc5bce5e75fab715efcf4a503
Rizxy Hilaludin <63449671+Rizxy@users.noreply.github.com>
2022-06-19 21:40:46 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Jun 20, 2022',
+  name: 'Rahmaniah Az-Zahrah'
 };
 
 function hideEverything() {
